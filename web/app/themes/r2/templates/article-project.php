5c8f0853fd01247cfdcb4d2b6156717184e17de4
poopsplat <matt.m.soria@gmail.com>
2019-11-18 13:45:29 -0600

    Adding color to the close button for team modals per issue #62

--- web/app/themes/r2/templates/article-project.php
+++ web/app/themes/r2/templates/article-project.php
@@ -2,7 +2,7 @@
 $title = $project->post_title;
 $title_first = get_post_meta($project->ID, '_cmb2_title_large', true);
 $title_second = get_post_meta($project->ID, '_cmb2_title_small', true);
-$image = Firebelly\Media\get_treated_image($project, ['size' => 'medium']);
+$image = Firebelly\Media\get_post_thumbnail($project);
 $location = \Firebelly\Utils\get_first_term($project, 'project_location');
 $property_type = \Firebelly\Utils\get_first_term($project, 'property_type');
 $status = get_post_meta($project->ID, '_cmb2_status', true);
@@ -11,7 +11,7 @@ $locality = get_post_meta($project->ID, '_cmb2_locality', true);
 <article class="project grid-item <?= $status ?> <?= $location->slug ?> <?= $property_type->slug ?> col-md-1-2 col-lg-1-4">
   <a class="-inner" href="<?= get_permalink($project); ?>">
 
-    <div class="project-image" <?= $image ?>></div>
+    <div class="project-image" style="background-image:url('<?= $image ?>');"></div>
     <div class="project-info">
       <h2 class="project-title">
         <?php if (!empty($title_first)): ?>
