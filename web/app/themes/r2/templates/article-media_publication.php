86705162bec35ae68950a9ca6a23abf4b70d8e2b
poopsplat <matt.m.soria@gmail.com>
2019-11-21 10:21:43 -0600

    Making some small adjustments to the media page styles based on client feedback

--- web/app/themes/r2/templates/article-media_publication.php
+++ web/app/themes/r2/templates/article-media_publication.php
@@ -8,6 +8,6 @@ $date = get_post_meta($media_publication->ID, '_cmb2_media_date', true);
 <?php if (!empty($link)): ?>
   <tr>
     <td class="publication"><?= $publication ?></td>
-    <td><a href="<?= $link ?>" target="_blank"><?= $title ?> <?= $date ?></a></td>
+    <td class="link"><a href="<?= $link ?>" target="_blank"><?= $title ?> <?= $date ?></a></td>
   </tr>
 <?php endif ?>
